6170449b9d78bc3c0bce15a79b349beceb215344
khwaishm <khwaishmehta1998@gmail.com>
2024-03-08 08:15:27 +0530

    added user and vidio model

--- src/models /user.model.js
+++ src/models /user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from 'mongoose'
+import { Jwt } from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
 //initializing the schema
 const userSchema = new Schema(
@@ -49,6 +51,50 @@ const userSchema = new Schema(
     timestamps: true,
   },
 )
+
+//using the pre method to hash the password before saving
+userSchema.pre('save', async function (next) {
+  // checking if the password is modified
+  if (!this.isModified('password')) return next()
+  //hashing the password
+  this.passward = bcrypt.hash(this.password, 10)
+  next()
+})
+
+//creating a custom method to compare the password
+
+userSchema.methods.isPasswordCorrect = async function (pass) {
+  return await bcrypt.compare(pass, this.passward)
+}
+
+// creating a custom method to generate the access token
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACESS_TOKEN_EXPIRY,
+    },
+  )
+}
+
+// creating a custom method to generate the refresh token
+userSchema.methods.generateRefreshToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    },
+  )
+}
 
 //exporting the model for user
 export const User = mongoose.model('User', userSchema)
--- src/models /video.model.js
+++ src/models /video.model.js
@@ -1,4 +1,60 @@
 import mongoose, { Schema } from 'mongoose'
-const videoSchema = new Schema({})
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2'
+
+const videoSchema = new Schema(
+  {
+    videofile: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    tags: {
+      type: [String],
+      required: true,
+    },
+    views: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    likes: {
+      type: Number,
+      required: true,
+    },
+    duration: {
+      type: Number, //duration in seconds
+      required: true,
+    },
+    isPublished: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
+    videoId: {
+      type: Schema.types.ObjectId,
+      ref: User,
+    },
+  },
+  {
+    timestamps: true,
+  },
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
 
 export const video = mongoose.model('video', videoSchema)
